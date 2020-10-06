4f6fe8540465b039a0661412b1e473b9f561e1ab
pierre claude <pierre.claude99@gmail.com>
2020-10-06 18:45:00 +0200

    ok

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,43 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+  def new
+    @cocktail = Cocktail.new
+  end
+  def create
+    @cocktail = Cocktail.new(cocktail_param)
+    if @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_param)
+    redirect_to cocktail_path(@cocktail)
+  end
+    def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_param
+    params.require(:cocktail).permit(:name)
+  end
+end
+
+
+
